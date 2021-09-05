566e16eb488e6f541afe36e1e231095be2a567ae
David Němeček <david.nemecek92@gmail.com>
2021-09-05 07:56:56 +0200

    Files uploaded

---
+++ app.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      userInput: '',
+      showGoals: true,
+    };
+  },
+
+  methods: {
+    toggleBtn() {
+      this.showGoals = !this.showGoals;
+    },
+    addGoal() {
+      if (this.goals.includes(this.userInput)) {
+        alert('This goal has been already added!');
+      } else {
+        this.goals.push(this.userInput);
+        this.userInput = '';
+      }
+    },
+    removeGoal(i) {
+      this.goals.splice(i, 1);
+    },
+  },
+});
+
+app.mount('#assignment');
